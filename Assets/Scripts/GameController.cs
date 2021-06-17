0764f975bd5425519160addf78395aad7efe5314
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-06-17 23:47:25 +0530

    Added restart button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
     public GameObject gameOverPanel;
+    public GameObject restartButton;
     public Text gameOverText;
 
     private string playerSide;
@@ -17,6 +18,7 @@ public class GameController : MonoBehaviour
         playerSide = "X";
         movesCount = 0;
         gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
         SetGameReferenceOnButtonsController();
     }
 
@@ -33,61 +35,88 @@ public class GameController : MonoBehaviour
         // Rows
         if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         else if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         // Columns
         else if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         else if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         else if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         // Diagonals
         else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
         else if (buttonList[6].text == playerSide && buttonList[4].text == playerSide && buttonList[2].text == playerSide)
         {
-            GameOver();
+            GameOver(playerSide);
         }
 
         if (movesCount >= 9)
         {
-            SetGameOverText("It's a draw!");
+            GameOver("draw");
         }
 
         // Change sides
         ChangeSides();
     }
 
-    void GameOver()
+    void GameOver(string winner)
     {
-        for (int i = 0; i < buttonList.Length; i++)
-        {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
-        }
+        SetBoardInteractable(false);
+        restartButton.SetActive(true);
 
-        SetGameOverText(playerSide + " Wins!");
+        if (winner == "draw")
+        {
+            SetGameOverText("It's a draw!");
+        }
+        else
+        {
+            SetGameOverText(playerSide + " Wins!");
+        }
     }
 
     public string GetPlayerSide()
     {
         return playerSide;
+    }
+
+    public void RestartGame()
+    {
+        playerSide = "X";
+        movesCount = 0;
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+
+        SetBoardInteractable(true);
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].text = "";
+        }
+    }
+
+    void SetBoardInteractable(bool toggle)
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+        }
     }
 
     void SetGameOverText(string text)

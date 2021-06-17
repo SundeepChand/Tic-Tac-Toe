12f593a4a23511c9356557e4feea7f37b3fd4130
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-06-17 21:41:59 +0530

    Initial Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+
+    void Awake()
+    {
+        SetGameReferenceOnButtonsController();
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("Endturn is not implemented");
+    }
+
+    public string GetPlayerSide()
+    {
+        return "?";
+    }
+
+    void SetGameReferenceOnButtonsController()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+}

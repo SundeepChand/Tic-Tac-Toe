0e06b69dacff22ff213bee6f80145afddfd0e2f3
Sundeep Chand <sundeep.chand2001@gmail.com>
2021-06-17 22:03:17 +0530

    Done basic implementation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,19 +7,73 @@ public class GameController : MonoBehaviour
 {
     public Text[] buttonList;
 
+    private string playerSide;
+
     void Awake()
     {
+        playerSide = "X";
         SetGameReferenceOnButtonsController();
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X" ? "O" : "X");
     }
 
     public void EndTurn()
     {
-        Debug.Log("Endturn is not implemented");
+        // Check winning conditions
+        // Rows
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        // Columns
+        else if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        // Diagonals
+        else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[6].text == playerSide && buttonList[4].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        // Change sides
+        ChangeSides();
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
     }
 
     public string GetPlayerSide()
     {
-        return "?";
+        return playerSide;
     }
 
     void SetGameReferenceOnButtonsController()
